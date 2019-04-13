ef08238b5000b11e9825ee212977798d0c9c95f2
Shehanka <hcsperera@outlook.com>
2019-04-13 13:28:46 +0530

    Model class added

---
+++ src/com/chamodshehanka/heshanhardware/model/Customer.java
@@ -0,0 +1,15 @@
+package com.chamodshehanka.heshanhardware.model;
+
+/**
+ * @author chamodshehanka on 4/13/2019
+ * @project HeshanHardware
+ **/
+public class Customer {
+
+    private String customerID;
+    private String name;
+    private String gender;
+    private String address;
+    private int phone;
+
+}
