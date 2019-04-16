4c5181cd8b1b44bf662aaa823610314027ef8219
Shehanka <hcsperera@outlook.com>
2019-04-16 20:36:18 +0530

    Web xml conf

---
+++ src/com/chamodshehanka/heshanhardware/servlet/LoginServlet.java
@@ -0,0 +1,25 @@
+package com.chamodshehanka.heshanhardware.servlet;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author chamodshehanka on 4/16/2019
+ * @project HeshanHardware
+ **/
+@WebServlet(name = "login")
+public class LoginServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse){
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse){
+
+    }
+
+}
