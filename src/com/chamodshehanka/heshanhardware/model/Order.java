6c8df7ac8dbb0024228726f147e6da90a3e12cc9
Shehanka <hcsperera@outlook.com>
2019-05-03 23:04:36 +0530

    Model class update

--- src/com/chamodshehanka/heshanhardware/model/Order.java
+++ src/com/chamodshehanka/heshanhardware/model/Order.java
@@ -1,8 +1,78 @@
 package com.chamodshehanka.heshanhardware.model;
 
+import java.util.ArrayList;
+
 /**
  * @author chamodshehanka on 4/13/2019
  * @project HeshanHardware
  **/
 public class Order {
+    private String orderID;
+    private String orderDate;
+    private String customerID;
+    private int orderDiscount;
+
+    private ArrayList<OrderDetail> orderDetailArrayList;
+
+    public Order() {
+    }
+
+    public Order(String orderID, String orderDate, String customerID, int orderDiscount, ArrayList<OrderDetail> orderDetailArrayList) {
+        this.orderID = orderID;
+        this.orderDate = orderDate;
+        this.customerID = customerID;
+        this.orderDiscount = orderDiscount;
+        this.orderDetailArrayList = orderDetailArrayList;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "orderID='" + orderID + '\'' +
+                ", orderDate='" + orderDate + '\'' +
+                ", customerID='" + customerID + '\'' +
+                ", orderDiscount=" + orderDiscount +
+                ", orderDetailArrayList=" + orderDetailArrayList +
+                '}';
+    }
+
+    public String getOrderID() {
+        return orderID;
+    }
+
+    public void setOrderID(String orderID) {
+        this.orderID = orderID;
+    }
+
+    public String getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(String orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public String getCustomerID() {
+        return customerID;
+    }
+
+    public void setCustomerID(String customerID) {
+        this.customerID = customerID;
+    }
+
+    public int getOrderDiscount() {
+        return orderDiscount;
+    }
+
+    public void setOrderDiscount(int orderDiscount) {
+        this.orderDiscount = orderDiscount;
+    }
+
+    public ArrayList<OrderDetail> getOrderDetailArrayList() {
+        return orderDetailArrayList;
+    }
+
+    public void setOrderDetailArrayList(ArrayList<OrderDetail> orderDetailArrayList) {
+        this.orderDetailArrayList = orderDetailArrayList;
+    }
 }
--- src/com/chamodshehanka/heshanhardware/model/OrderDetail.java
+++ src/com/chamodshehanka/heshanhardware/model/OrderDetail.java
@@ -5,4 +5,60 @@ package com.chamodshehanka.heshanhardware.model;
  * @project HeshanHardware
  **/
 public class OrderDetail {
+    private String orderID;
+    private String itemCode;
+    private int itemQty;
+    private double itemUnitPrice;
+
+    public OrderDetail() {
+    }
+
+    public OrderDetail(String orderID, String itemCode, int itemQty, double itemUnitPrice) {
+        this.orderID = orderID;
+        this.itemCode = itemCode;
+        this.itemQty = itemQty;
+        this.itemUnitPrice = itemUnitPrice;
+    }
+
+    @Override
+    public String toString() {
+        return "OrderDetail{" +
+                "orderID='" + orderID + '\'' +
+                ", itemCode='" + itemCode + '\'' +
+                ", itemQty=" + itemQty +
+                ", itemUnitPrice=" + itemUnitPrice +
+                '}';
+    }
+
+    public String getOrderID() {
+        return orderID;
+    }
+
+    public void setOrderID(String orderID) {
+        this.orderID = orderID;
+    }
+
+    public String getItemCode() {
+        return itemCode;
+    }
+
+    public void setItemCode(String itemCode) {
+        this.itemCode = itemCode;
+    }
+
+    public int getItemQty() {
+        return itemQty;
+    }
+
+    public void setItemQty(int itemQty) {
+        this.itemQty = itemQty;
+    }
+
+    public double getItemUnitPrice() {
+        return itemUnitPrice;
+    }
+
+    public void setItemUnitPrice(double itemUnitPrice) {
+        this.itemUnitPrice = itemUnitPrice;
+    }
 }
