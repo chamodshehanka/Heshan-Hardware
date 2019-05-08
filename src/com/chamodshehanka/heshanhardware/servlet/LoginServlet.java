7d97f04eb8f9fc4853a6c8622d9bd78a1a202117
Shehanka <hcsperera@outlook.com>
2019-05-08 14:21:04 +0530

    update

--- src/com/chamodshehanka/heshanhardware/servlet/LoginServlet.java
+++ src/com/chamodshehanka/heshanhardware/servlet/LoginServlet.java
@@ -1,15 +1,23 @@
 package com.chamodshehanka.heshanhardware.servlet;
 
+import com.chamodshehanka.heshanhardware.model.User;
+import com.chamodshehanka.heshanhardware.service.custom.UserService;
+import com.chamodshehanka.heshanhardware.service.custom.impl.UserServiceImpl;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
 
 /**
  * @author chamodshehanka on 4/16/2019
  * @project HeshanHardware
  **/
-@WebServlet(name = "login")
+@WebServlet(name = "login", urlPatterns = "/login")
 public class LoginServlet extends HttpServlet {
 
     @Override
@@ -18,8 +26,26 @@ public class LoginServlet extends HttpServlet {
     }
 
     @Override
-    protected void doPost(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse){
+    protected void doPost(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws ServletException, IOException {
+        UserService userService = new UserServiceImpl();
+        ArrayList<User> userArrayList = userService.getAll();
 
+        String username = httpServletRequest.getParameter("username");
+        String password = httpServletRequest.getParameter("password");
+
+        boolean isValidUser = false;
+
+        for (User user : userArrayList) {
+            if (user.getUserName().equals(username) && user.getUserPassword().equals(password)){
+                isValidUser = true;
+            }
+        }
+
+        if (isValidUser){
+            RequestDispatcher requestDispatcher = getServletContext()
+                    .getRequestDispatcher("/dashboard.jsp");
+            requestDispatcher.forward(httpServletRequest, httpServletResponse);
+        }
     }
 
 }
