d6bbe4a11614e174b926a17716ba3d6cd80fc371
Chamod Shehanka Perera <hcsperera@outlook.com>
2019-05-14 15:57:19 +0530

    Customer Making

--- src/com/chamodshehanka/heshanhardware/service/custom/impl/CustomerServiceImpl.java
+++ src/com/chamodshehanka/heshanhardware/service/custom/impl/CustomerServiceImpl.java
@@ -2,9 +2,18 @@ package com.chamodshehanka.heshanhardware.service.custom.impl;
 
 import com.chamodshehanka.heshanhardware.model.Customer;
 import com.chamodshehanka.heshanhardware.service.custom.CustomerService;
+import com.chamodshehanka.heshanhardware.util.CommonConstants;
+import com.chamodshehanka.heshanhardware.util.DBConnectionUtil;
 import com.chamodshehanka.heshanhardware.util.IDGenerator;
+import com.chamodshehanka.heshanhardware.util.QueryUtil;
+import org.xml.sax.SAXException;
 
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 
 /**
@@ -13,8 +22,37 @@ import java.util.ArrayList;
  **/
 public class CustomerServiceImpl implements CustomerService {
 
+    private static Connection connection;
+    private Statement statement;
+    private PreparedStatement preparedStatement;
+
     @Override
     public boolean add(Customer customer) {
+        try{
+            connection = DBConnectionUtil.getDBConnection();
+            preparedStatement = connection.prepareStatement(QueryUtil.queryByID(CommonConstants.QUERY_ID_INSERT_CUSTOMER));
+
+            preparedStatement.setObject(CommonConstants.COLUMN_INDEX_ONE, customer.getCustomerID());
+            preparedStatement.setObject(CommonConstants.COLUMN_INDEX_TWO, customer.getName());
+            preparedStatement.setObject(CommonConstants.COLUMN_INDEX_THREE, customer.getGender());
+            preparedStatement.setObject(CommonConstants.COLUMN_INDEX_FOUR, customer.getAddress());
+            preparedStatement.setObject(CommonConstants.COLUMN_INDEX_FIVE, customer.getPhone());
+
+            return 0 < preparedStatement.executeUpdate();
+        }catch (SQLException | ClassNotFoundException | SAXException | ParserConfigurationException | IOException e){
+            e.printStackTrace();
+        }finally {
+            try {
+                if (preparedStatement != null) {
+                    preparedStatement.close();
+                }
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
         return false;
     }
 
